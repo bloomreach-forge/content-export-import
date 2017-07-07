5b523b0041da0c06af502086c918b767351f00e4
Woonsan Ko <woonsan.ko@bloomreach.com>
2017-07-07 12:53:28 -0400

    Removing demo to recreate

--- demo/site/src/main/java/org/onehippo/forge/content/exim/demo/beans/BaseDocument.java
+++
@@ -1,9 +0,0 @@
-package org.onehippo.forge.content.exim.demo.beans;
-
-import org.hippoecm.hst.content.beans.Node;
-import org.hippoecm.hst.content.beans.standard.HippoDocument;
-
-@Node(jcrType="contenteximdemo:basedocument")
-public class BaseDocument extends HippoDocument {
-
-}
