81295d2e1f28a01152d738841ea9829cfd9c4f1e
Woonsan Ko <woonsan.ko@bloomreach.com>
2017-07-07 13:03:13 -0400

    Adding a new demo.

---
+++ demo/site/src/main/java/org/onehippo/forge/content/exim/demo/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package org.onehippo.forge.content.exim.demo.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="contenteximdemo:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
